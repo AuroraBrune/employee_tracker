a2da61a7a10f332d6da33b3f16caccf459d1e5e9
Aurora Brune <akbrune@hotmail.com>
2020-08-28 14:32:46 -0500

    mysql in, database created

---
+++ employees-seed.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeesDB;
+
+CREATE DATABASE employeesDB;
+
+USE employeesDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+ );
+
+ CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT ,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT ,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
